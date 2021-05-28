8b49b56e15bc46be6727428985dfd2f935dd009a
devdeadviz <kuldeepgupta485@gmail.com>
2021-05-28 18:32:59 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+replaceText(document.body);
+
+function replaceText(element) {
+  if (element.hasChildNodes()) {
+    element.childNodes.forEach(replaceText);
+  } else if (element.nodeType === Text.TEXT_NODE) {
+    if (element.textContent.match(/coronavirus/gi)) {
+      /* CoronaVirus Booming Code */
+      //   const newElement = document.createElement("span");
+      //   newElement.innerHTML = element.textContent.replace(
+      //     /(coronavirus)/gi,
+      //     '<span class="rainbow" >$1</span>'
+      //   );
+      //   element.replaceWith(newElement);
+
+      /* CoronaVirus Removing Code */
+      element.parentElement.remove();
+    }
+  }
+}
